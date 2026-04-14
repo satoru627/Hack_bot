c198335e0ea0dc17ad04d75f1a8182feefb4caca
SATORU <emmanuelstephane671@gmail.com>
2026-04-14 09:00:29 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -10,9 +10,9 @@ export async function pingTest(client, message) {
 
     await client.sendMessage(remoteJid, {
         text: stylizedChar(
-            `🚀 Digital Crew Network\n\n` +
+            `🚀 Satoru_Gj Network\n\n` +
             `Latency: ${latency} ms\n\n` +
-            `Digital Crew 243`
+            `satoru`
         )
     }, { quoted: message })
 }
