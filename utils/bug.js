ebc0477dac99f6f02df35ddac37bbf73cba83dd0
SATORU <emmanuelstephane671@gmail.com>
2026-04-14 08:15:52 +0100

    Update bug.js

--- utils/bug.js
+++ utils/bug.js
@@ -7,7 +7,7 @@ async function bug(message, client, texts, num) {
             contextInfo: {
                 externalAdReply: {
                     title: "Join Our WhatsApp Channel",
-                    body: " 𓆩 𝐃𝐢𝐠𝐢𝐭𝐚𝐥 𝐂𝐫𝐞𝐰 𝟐𝟒𝟑 𓆪 ",
+                    body: " SATORU",
                     mediaType: 1,
                     thumbnailUrl: `https://whatsapp.com/channel/0029VbBT7FdLCoX1TDyQQb1B`,
                     renderLargerThumbnail: false,
