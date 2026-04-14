69e933e936bf384e26670442dc61489e30d4a6eb
SATORU <emmanuelstephane671@gmail.com>
2026-04-14 08:57:24 +0100

    Update img.js

--- commands/img.js
+++ commands/img.js
@@ -57,7 +57,7 @@ async function img(message, client) {
                     caption:
                         `📷 ${query}\n` +
                         `${image.title && image.title !== "No title" ? image.title + "\n" : ""}` +
-                        `© Digital Crew 243`
+                        `© satoru_Gj`
                 });
 
                 await new Promise(r => setTimeout(r, 1000));
